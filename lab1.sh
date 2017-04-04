c368596c63406bfb101252c4be183cc29ea791a9
Joseph <jbeckcastro810@southseattle.edu>
2017-04-04 15:13:10 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -8,12 +8,18 @@
 
 # Linux Commands for 
 
-#whoami         tells me which user I am logged in as (who)
+#whoami      tells me which user I am logged in as (who)
 
 #pwd         pwd stands for print working directory and tells me where I am (where)
 
-#ls        ls Tells me which files are in the directory (what)
+#ls          ls Tells me which files are in the directory (what)
 
-#cd  cd allows me to change to other directories (navigation)
+#cd          cd allows me to change to other directories (navigation)
  
-#date         what time is it on the machine (when)
+#date        what time is it on the machine (when)
+
+
+/var/root # cat test.sh                                                         
+!#/bin/sh                                                                       
+                                                                                
+echo "something fun" 

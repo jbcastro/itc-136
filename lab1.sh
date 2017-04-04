4a27bca6b47d6959a0b82c4d93523b566744021c
Joseph <jbeckcastro810@southseattle.edu>
2017-04-04 14:47:50 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -5,8 +5,15 @@
 #130.211.233.99
 
 
-#Linux Commands for 
-    #Who  whoami
-    #where pwd
-    #what ls
-    #navigation cd
+
+# Linux Commands for 
+
+#WHO  whoami tells me which user I am logged in as (who)
+
+#WHERE pwd stands for print working directory and tells me where I am (where)
+
+#WHAT ls Tells me which files are in the directory (what)
+
+#NAVIGATION cd allows me to change to other directories (navigation)
+ 
+#WHEN what time is it on the machine (when)
